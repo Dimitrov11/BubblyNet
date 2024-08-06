4e5a2d1a02e2650c6375786f5a1563f27127a0c3
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-08-06 12:12:58 +0300

    Inital commit for init Router

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+
 function Navigation() {
   return (
     <>
       <p>Hello</p>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Navigation from './Navigation.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navigation />
+    <Router>
+        <Navigation />
+    </Router>
   </React.StrictMode>,
 )
