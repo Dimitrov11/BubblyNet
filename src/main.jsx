2b4a9a33b8b9a464ca83c7b9011e94062c4e5fdd
Dimitar Dimitrov <dimitrov.georgiev91@gmail.com>
2024-08-06 17:18:49 +0300

    Firebase init

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Navigation from './Navigation.jsx'
+import app from './firebase.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Navigation />
     </Router>
   </React.StrictMode>,
-)
+);
